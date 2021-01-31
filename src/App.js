bfc51afe2a6c3da75c2413a18981ad06f1a3d540
Akhil Waghmare <akhilwaghmare@gmail.com>
2021-01-31 08:54:06 -0500

    Starter code

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
-/** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react'
-
-import { useEffect, useRef } from 'react'
-import { TweenLite } from 'gsap'
-
 import './App.css';
-import Gallery from './components/Gallery';
-import Navbar from './components/Navbar';
-
-import data from './assets/data';
-
-const appStyles = css`
-display: flex;
-height: 100vh;
-align-items: stretch;
-
-visibility: hidden;
-`
 
 function App() {
-  let app = useRef(null)
-
-  useEffect(() => {
-    TweenLite.to(app, 0, {
-      css: {
-        visibility: 'visible'
-      }
-    })
-  }, [])
-
   return (
-    <div className="App" css={appStyles} ref={el => app = el}>
-      <Navbar />
-      <Gallery data={data} />
+    <div className="App">
+      Sliding Image Gallery
     </div>
   );
 }
